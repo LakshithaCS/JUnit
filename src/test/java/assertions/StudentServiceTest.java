26c1f84564b971c26ee0ea73dd7ecbc72527aa17
LakshithaCS <rlakshitha@isa.ae>
2024-11-30 21:14:14 +0530

    AssertFalse

--- src/test/java/assertions/StudentServiceTest.java
+++ src/test/java/assertions/StudentServiceTest.java
@@ -2,6 +2,7 @@ package assertions;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StudentServiceTest {
@@ -17,5 +18,18 @@ class StudentServiceTest {
         assertTrue(() -> actual, "Student list is not empty"); // boolean supplier + with error message
         assertTrue(actual, () -> "Student list is not empty"); // String supplier for error message
         assertTrue(() -> actual, () -> "Student list is not empty"); // boolean supplier + String supplier for error message
+    }
+
+    @Test
+    void testGetStudents_assert_false() {
+        StudentService studentService = new StudentService();
+        boolean actual = !studentService.getStudents().isEmpty();
+
+        assertFalse(actual);
+        assertFalse(() -> actual); // boolean supplier
+        assertFalse(actual, "Student list is not empty"); // with error message
+        assertFalse(() -> actual, "Student list is not empty"); // boolean supplier + with error message
+        assertFalse(actual, () -> "Student list is not empty"); // String supplier for error message
+        assertFalse(() -> actual, () -> "Student list is not empty"); // boolean supplier + String supplier for error message
     }
 }
