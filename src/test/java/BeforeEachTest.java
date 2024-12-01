dfa6ad00e96ff75123a0bc0eea4a9265e555f742
LakshithaCS <rlakshitha@isa.ae>
2024-12-01 16:47:29 +0530

    BeforeEach

---
+++ src/test/java/BeforeEachTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BeforeEachTest {
+
+    Calculator calculator;
+
+    @BeforeEach
+    void init() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("Test add method")
+    void testAdd() {
+        int actualResult = calculator.add(2, 3);
+
+        assertEquals(5, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test subtract method")
+    void testSubtract() {
+        int actualResult = calculator.subtract(5, 3);
+
+        assertEquals(2, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test multiply method")
+    void testMultiply() {
+        int actualResult = calculator.multiply(2, 3);
+
+        assertEquals(6, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test divide method")
+    void testDivide() {
+        int actualResult = calculator.divide(6, 3);
+
+        assertEquals(2, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test divide method by zero")
+    void testDivideByZero() {
+        ArithmeticException exception = assertThrows(ArithmeticException.class, () -> calculator.divide(6, 0));
+
+        assertEquals("Cannot divide by zero", exception.getMessage());
+    }
+}
