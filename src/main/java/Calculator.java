398ba9e6bd4fac55ed73a637ce49daaaf51d7208
LakshithaCS <rlakshitha@isa.ae>
2024-12-01 16:15:07 +0530

    DisplayName

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,4 +4,22 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    //method for subtracting two numbers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    //method for multiplying two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    //method for dividing two numbers
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,14 +1,53 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Calculator operation tests")
 class CalculatorTest {
 
     @Test
+    @DisplayName("Test add method")
     void testAdd() {
         Calculator calculator = new Calculator();
         int actualResult = calculator.add(2, 3);
 
         assertEquals(5, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test subtract method")
+    void testSubtract() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.subtract(5, 3);
+
+        assertEquals(2, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test multiply method")
+    void testMultiply() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.multiply(2, 3);
+
+        assertEquals(6, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test divide method")
+    void testDivide() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.divide(6, 3);
+
+        assertEquals(2, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test divide method by zero")
+    void testDivideByZero() {
+        Calculator calculator = new Calculator();
+        ArithmeticException exception = assertThrows(ArithmeticException.class, () -> calculator.divide(6, 0));
+
+        assertEquals("Cannot divide by zero", exception.getMessage());
     }
 }
